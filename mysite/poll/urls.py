04976344fbbb4334f965880b39123240e49976ea
MohamedAMha <mohamed.a.mhaidar@gmail.com>
2025-07-21 17:10:29 -0700

    A lot of errors in views.py And there is too much to wrap my head around when it comes to template form writing

--- mysite/poll/urls.py
+++ mysite/poll/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 # at end of django part 3, I skipped the part about re-writing hard-coded urls
 
+app_name = "poll"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/result", views.result, name="result"),
--- mysite/poll/views.py
+++ mysite/poll/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Question
 
 def index(request):
@@ -10,11 +12,23 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
 
 def result(request, question_id):
     response = " These are the responses to the question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("you're responding to the question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        # where is .choice_set coming from
+    except (KeyError, Choice.DoesNotExist):
+        # pycharm is telling me Choice is unresolved reference,
+        # but I assumed it was a reference to something in the database
+        return render(request, "polls/details.html", {"question":question,"error message":"You didn't select a choice.",},)
+        # long as hell, but apparently the exception should still return a specified error message
+    else:
+        selected_choice.votes = F("Votes") + 1
+        selected_choice.save()
