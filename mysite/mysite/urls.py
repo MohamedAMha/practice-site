8878474629ef53d3267a1f472a77d1f3541c6a50
MohamedAMha <mohamed.a.mhaidar@gmail.com>
2025-07-24 13:40:54 -0700

    I can't believe the index.html error was just "poll:detail" instead of "detail"

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('/poll/index.html')),
     path('poll/', include("poll.urls")),
     path('admin/', admin.site.urls),
 ]
