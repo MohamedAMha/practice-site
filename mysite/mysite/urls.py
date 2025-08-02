d7959b4e8b74f9c6311c9b89fb80cb94956b733b
MohamedAMha <mohamed.a.mhaidar@gmail.com>
2025-08-02 07:05:45 -0700

    I'm done fucking around with this project. Gonna switch over to the 2nd tutorial to learn more about templating.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include("poll.urls")),  # handles base url of 127.0.0.1.8000
     path('poll/', include("poll.urls")),
     path('admin/', admin.site.urls),
 ]
--- mysite/poll/views.py
+++ mysite/poll/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
     template = "poll/index.html"
+    context = {"latest_question_list": latest_question_list}
     return render(request, template, context)
 
 def detail(request, question_id):
